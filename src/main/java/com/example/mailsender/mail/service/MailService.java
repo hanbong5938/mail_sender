e225bea4d6db989672afac9eaa7e3b51054d76a8
hanbong5938 <51283645+hanbong5938@users.noreply.github.com>
2021-07-29 15:50:17 +0900

    Update MailService.java

--- src/main/java/com/example/mailsender/mail/service/MailService.java
+++ src/main/java/com/example/mailsender/mail/service/MailService.java
@@ -5,5 +5,5 @@ import com.example.mailsender.mail.dto.request.MailRequest;
 public interface MailService {
     boolean send(MailRequest mailRequest);
 
-    void getMail(String title);
+    void getMail();
 }
